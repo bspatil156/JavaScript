cad7a3e478d7c9a13e8b7e3d3739099cb277bd91
bspatil156 <mr.bspatil156@gmail.com>
2024-02-20 00:11:29 +0530

    IIFE

--- IIFE.js
+++ IIFE.js
@@ -6,12 +6,24 @@
 // Though execution parenthesis are not mentioned, if IIFE further is written,
 // the function is executed
 (function Print1(){
+    // Named IIFE
     console.log(`DB Connection Successfull!!!`)
-})
+})();
 
 
 // A semi-colon must be included in IIFE syntax as mentioned above
 // Else '(intermediate value)(...)' error occurs
 (function Print2(){
+    // Named IIFE
     console.log(`Welcome to the project!!!`)
-})()
+})();
+
+// IIFE of Arrow function
+(() => {
+    // un-named IIFE
+    console.log(`IIFE Of Arrow Function`)
+})();
+
+((name) => `Hello ${name}`)(console.log("Bhavesh"))
+
+
