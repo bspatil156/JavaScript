585c64e27651e4239464b50824d023ceab928576
bspatil156 <mr.bspatil156@gmail.com>
2024-02-23 23:50:03 +0530

    Filter fully completed

--- Loops/Filter.js
+++ Loops/Filter.js
@@ -8,7 +8,9 @@ let values = arr.forEach( (element) =>{
 
 console.log(values)
 
+// filter() method : 
 // filter returns the value that is mentioned in the call 
+// filter checks the condition and return the array of elements that proove the condition as true
 let fillter = arr.filter((element) =>{
     return element
 })
@@ -67,3 +69,19 @@ console.log(philosophy)
 
 let mrssm = obj.filter( (element) => element.author == "Dr.Sudha Murthy")
 console.log(mrssm)
+
+
+// filter does'nt return any array operation performed on it 
+let myarr = [1,2,3,4,5,6,7,8,9,10]
+let number = myarr.filter( (num) => num * 2)
+console.log(number)
+
+let num = myarr.filter( (num) => {
+    if(num>3){
+        return (num * 4)
+    }
+    else{
+        return (num * 2)
+    }
+})
+console.log(num)
