73d39af102971157e2b25209b3a8aa69f5376fbc
bspatil156 <mr.bspatil156@gmail.com>
2024-02-26 23:51:34 +0530

    Map partially completed

--- Loops/Map.js
+++ Loops/Map.js
@@ -6,3 +6,7 @@ let arr2 = ["Rolls Royce","Mercedes","Audi","Mustang"]
 let nums2 = arr2.map( (num) => `${num} is a good car`)
 console.log(nums2)
 
+let arr3 = [{name1 : "Bhavesh"},{name2 : "Dhiraj"},{ name3 : "Pratik"},{name4 : "Sahil"}]
+let nums3 = arr3.map( (num) => `${num} contains the name of a boy!!!`)
+console.log(nums3)
+
