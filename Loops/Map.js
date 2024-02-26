b8374777c9c190593b163f6a6188f8e2d72089ed
bspatil156 <mr.bspatil156@gmail.com>
2024-02-26 23:47:13 +0530

    Map partially completed

--- Loops/Map.js
+++ Loops/Map.js
@@ -1,4 +1,8 @@
-let arr = [1,2,3,4,5,6,7,8,9,10]
-let nums = arr.map( (num) => num * 4)
-console.log(nums)
+let arr1 = [1,2,3,4,5,6,7,8,9,10]
+let nums1 = arr1.map( (num) => num * 4)
+console.log(nums1)
+
+let arr2 = ["Rolls Royce","Mercedes","Audi","Mustang"]
+let nums2 = arr2.map( (num) => `${num} is a good car`)
+console.log(nums2)
 
