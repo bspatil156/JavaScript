ff794f119c6e6ca1031bfb41d7d6287a9a8d3baa
bspatil156 <mr.bspatil156@gmail.com>
2024-03-07 23:26:25 +0530

    Reduce Started

--- Loops/Reduce.js
+++ Loops/Reduce.js
@@ -1,6 +1,14 @@
 let arr = [1,2,3,4,5,6]
-let initialValue = 0
-let reduce = arr.reduce((accumulator,currentValue) => {
+let initialValue1 = 0
+let reduce1 = arr.reduce((accumulator,currentValue) => {
     return accumulator + currentValue 
-},initialValue)
-console.log(reduce)
+},initialValue1)
+console.log(reduce1)
+
+let initialValue2 = 1
+let reduce2 = arr.reduce( (accumulator,currentValue) =>{
+    return (currentValue ** 2)
+},initialValue2)
+console.log(reduce2)
+
+console.log(21/6)
